c8f551a1f6957fab3e5ccc87ed285ef7b02ad4f8
Pr4veenn <praveen.tech.js@gmail.com>
2026-02-25 11:51:53 +0530

    ci cd pipeline updates

--- src/infrastructure/config/socket.ts
+++ src/infrastructure/config/socket.ts
@@ -1,10 +1,14 @@
 import { Server } from "socket.io";
 import { Server as HttpServer } from "http";
+const CLIENT_URL = process.env.FRONTEND_URL
 
 export const createSocketServer = (httpServer: HttpServer)=>{
     const io = new Server(httpServer,{
         cors:{
-            origin: 'http://localhost:5173',
+            origin: [
+                'http://localhost:5173',
+                'https://beatbay.online'
+            ],
             credentials: true
         }
     })
